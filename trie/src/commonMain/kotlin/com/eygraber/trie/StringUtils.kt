968e1de112d7a5633d86b4a97844d736a0a5f466
Eliezer Graber <me@eygraber.com>
2026-01-28 00:12:49 -0500

    Update Detekt v2 to alpha 2

--- trie/src/commonMain/kotlin/com/eygraber/trie/StringUtils.kt
+++ trie/src/commonMain/kotlin/com/eygraber/trie/StringUtils.kt
@@ -3,7 +3,7 @@ package com.eygraber.trie
 internal fun String.commonPrefixLength(other: String, otherOffset: Int = 0): Int {
   val minLength = minOf(this.length, other.length - otherOffset)
   for(i in 0 until minLength) {
-    if (this[i] != other[i + otherOffset]) {
+    if(this[i] != other[i + otherOffset]) {
       return i
     }
   }
